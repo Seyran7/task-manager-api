05addd7af7990be09b501243f50d914a9d8c906c
seyran7 <brsr888@gmail.com>
2026-03-19 16:27:34 +0400

    Add TaskController

---
+++ src/main/java/com/seyran/taskmanager/controller/TaskController.java
@@ -0,0 +1,29 @@
+package com.seyran.taskmanager.controller;
+
+import com.seyran.taskmanager.entity.Task;
+import com.seyran.taskmanager.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class TaskController {
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task){
+        return ResponseEntity.ok(taskService.createTask(task));
+    }
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks(){
+        return ResponseEntity.ok(taskService.findAll());
+    }
+    @DeleteMapping
+    public void deleteTask(@RequestParam Long id){
+        taskService.deleteTask(id);
+    }
+}
