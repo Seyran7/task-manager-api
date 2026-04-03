8375376782436f9b2e86f4c622e0dcf619f7169e
seyran7 <brsr888@gmail.com>
2026-04-03 16:07:02 +0400

    Add User class and UserRepository interface

---
+++ src/main/java/com/seyran/taskmanager/entity/User.java
@@ -0,0 +1,21 @@
+package com.seyran.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+}
