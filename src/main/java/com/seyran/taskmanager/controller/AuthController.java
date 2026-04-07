10bd55d78115f84141c91119be3320c224e73ab5
seyran7 <brsr888@gmail.com>
2026-04-07 23:39:42 +0400

    Add AuthController class

---
+++ src/main/java/com/seyran/taskmanager/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.seyran.taskmanager.controller;
+
+import com.seyran.taskmanager.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public String register(@RequestParam String username,
+                           @RequestParam String password){
+        return authService.register(username, password);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password){
+        return authService.login(username, password);
+    }
+}
