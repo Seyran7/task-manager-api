0a7b05b4711a3be7d11c141108f7ba5280c58e89
seyran7 <brsr888@gmail.com>
2026-05-06 23:31:26 +0400

    Add CustomAccessDeniedHandler class

--- src/main/java/com/seyran/taskmanager/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/seyran/taskmanager/security/CustomAccessDeniedHandler.java
@@ -3,26 +3,26 @@ package com.seyran.taskmanager.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.seyran.taskmanager.dto.ApiResponse;
 import jakarta.servlet.http.*;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
-public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
-    public void commence(HttpServletRequest request,
-                         HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
 
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType("application/json");
 
         ApiResponse<?> apiResponse = ApiResponse.builder()
                 .success(false)
-                .message("Unauthorized: Invalid or missing token")
+                .message("Forbidden: You don't have permission")
                 .data(null)
                 .build();
 
