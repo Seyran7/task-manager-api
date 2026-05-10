191ced69c4bb7946d2edf290b98c946ad5077b58
seyran7 <brsr888@gmail.com>
2026-05-10 15:21:19 +0400

    Update JwtService clasas

--- src/main/java/com/seyran/taskmanager/security/JwtService.java
+++ src/main/java/com/seyran/taskmanager/security/JwtService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -60,5 +61,25 @@ public class JwtService {
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
+    }
+    public boolean isTokenValid(String token, String username) {
+
+        final String extractedUsername = extractUsername(token);
+
+        return extractedUsername.equals(username) && !isTokenExpired(token);
+    }
+    private boolean isTokenExpired(String token) {
+
+        return extractExpiration(token).before(new Date());
+    }
+    private Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
+    }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+
+        final Claims claims = extractAllClaims(token);
+
+        return claimsResolver.apply(claims);
     }
 }
